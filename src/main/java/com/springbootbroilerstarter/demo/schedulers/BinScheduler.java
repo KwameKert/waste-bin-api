b572046625044ba6232c1b54fa72f016311b8abf
kwamekert <kwamekert@gmail.com>
2021-11-16 07:22:31 +0000

    updated fields

--- src/main/java/com/springbootbroilerstarter/demo/schedulers/BinScheduler.java
+++ src/main/java/com/springbootbroilerstarter/demo/schedulers/BinScheduler.java
@@ -1,6 +1,7 @@
 package com.springbootbroilerstarter.demo.schedulers;
 
 import com.springbootbroilerstarter.demo.dtos.ChannelFeed;
+import com.springbootbroilerstarter.demo.dtos.Feed;
 import com.springbootbroilerstarter.demo.services.interfaces.BinService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,17 +19,25 @@ public class BinScheduler {
 
     private static final Logger LOG = LoggerFactory.getLogger(BinScheduler.class);
 
-    @Scheduled(fixedRate = 30000000)
+    @Scheduled(fixedDelay = 5000)
     public void checkBinFull(){
         RestTemplate restTemplate = new RestTemplate();
         String thinkSpeakUrl
                 = "https://api.thingspeak.com/channels/1545848/feeds.json?results=1";
         ResponseEntity<ChannelFeed> response
                 = restTemplate.getForEntity(thinkSpeakUrl , ChannelFeed.class);
-        int binOneStatus = Integer.parseInt(response.getBody().feeds.get(0).field1) <= 33 ? 1: 0;
-        int binOTwoStatus = Integer.parseInt(response.getBody().feeds.get(0).field2) <= 33 ? 1: 0;
-        this.binService.updateBinSensor(binOneStatus, 1L);
-        this.binService.updateBinSensor(binOTwoStatus, 2L);
+       // System.out.println(response);
+        Feed feed = response.getBody().feeds.get(0);
+        if(feed.field1 != null){
+            System.out.println(feed.field1);
+            int binOneStatus = Integer.parseInt(response.getBody().feeds.get(0).field1) <= 33 ? 1: 0;
+            this.binService.updateBinSensor(binOneStatus, 1L);
+        }else if(feed.field2 != null){
+            System.out.println(feed.field2);
+            int binOneStatus = Integer.parseInt(response.getBody().feeds.get(0).field2) <= 33 ? 1: 0;
+            this.binService.updateBinSensor(binOneStatus, 2L);
+        }
+
         LOG.info("Fetching data from thinkspeak ---> "+response.getBody().channel.name);
     }
 
